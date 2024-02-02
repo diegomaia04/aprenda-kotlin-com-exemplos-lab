5edcd751b2c6f05c23f11284d46eab1d922447c4
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2024-02-02 15:37:08 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario {
+    override fun toString(): String {
+        return "Usuario(${hashCode()})"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60) {
+    fun alterarDuracao(novaDuracao: Int) {
+        duracao = novaDuracao
+    }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("$usuario matriculado na formação $nome.")
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
+
+    fun removerConteudo(conteudo: ConteudoEducacional) {
+        conteudos.remove(conteudo)
+    }
+
+    fun listarInscritos() {
+        inscritos.forEach {
+            println(it.toString())
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120)
+
+    val formacao = Formacao("Desenvolvimento Kotlin")
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    formacao.adicionarConteudo(conteudo1)
+    formacao.adicionarConteudo(conteudo2)
+
+    formacao.listarInscritos()
+
+    // Teste de alteração da duração do conteúdo educacional
+    conteudo1.alterarDuracao(120)
+
+    // Teste de remoção de conteúdo educacional
+    formacao.removerConteudo(conteudo2)
 }
